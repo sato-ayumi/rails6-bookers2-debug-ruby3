08e0c357227cfca312f179023254864868e267bb
sato-ayumi <129816397+sato-ayumi@users.noreply.github.com>
2023-05-10 13:08:31 +0000

    [Finish] 応用課題４

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,16 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  # フォローされる側の関係性
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 上記の関係を通じて参照（自分をフォローしている人）
+  has_many :followers, through: :followed, source: :follower
+  
+  # フォローする側の関係性
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 上記の関係を通じて参照（自分がフォローしている人）
+  has_many :followings, through: :follower, source: :followed
+  
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -16,5 +26,18 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  # コントローラーでも使用する定義
+  def follow(user)
+    follower.create(followed_id: user.id)
+  end
+  
+  def unfollow(user)
+    follower.find_by(follow_id: user.id).destroy
+  end 
+  
+  def following?(user)
+    followings.include?(user)
   end
 end
